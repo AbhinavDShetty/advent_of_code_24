3fb4c7b4702d8a1daf5f31cf7ea98bd18a233047
AbhinavDShetty <abhinavdshetty@gmail.com>
2024-12-01 16:09:35 +0530

    Puzzle1 and Puzzle2 Solutions

---
+++ puzzle_1/sol_1.py
@@ -0,0 +1,23 @@
+from itertools import product
+
+with open('C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\puzzle_1\\input_1.txt', 'r') as f:
+    list_raw_1 = f.readlines()
+
+with open('C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\puzzle_1\\input_2.txt', 'r') as f:
+    list_raw_2 = f.readlines()
+
+
+list_1 = [int(num_raw) for num_raw in list_raw_1]
+list_2 = [int(num_raw) for num_raw in list_raw_2]
+list_1.sort()
+list_2.sort()
+
+sum_diff = 0
+
+list_len = len(list_1)
+
+for i in range(list_len):
+    diff = abs(list_1[i] - list_2[i])
+    sum_diff += diff
+
+print(sum_diff)
---
+++ puzzle_2/sol_2.py
@@ -0,0 +1,16 @@
+with open('C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\puzzle_2\\input_1.txt', 'r') as f:
+    list_raw_1 = f.readlines()
+    
+with open('C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\puzzle_2\\input_2.txt', 'r') as f:
+    list_raw_2 = f.readlines()
+    
+list_1 = [int(num_raw) for num_raw in list_raw_1]
+list_2 = [int(num_raw) for num_raw in list_raw_2]
+
+total_sum = 0
+
+for num1 in list_1:
+    num_count = sum(1 for num2 in list_2 if num1 == num2)
+    total_sum += num_count * num1
+    
+print(total_sum)
