0f276a8703c271eaf0a90f942893c066e628195c
AbhinavDShetty <abhinavdshetty@gmail.com>
2024-12-03 00:04:04 +0530

    Day 2 Solution Updated
    Updated solution for puzzle3 and puzzle4 day2

--- puzzle_4/sol_4.py
+++ puzzle_4/sol_4.py
@@ -1,21 +1,28 @@
-with open("C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\puzzle_4\\input.txt", "r") as f: 
-    data_raw = f.read() 
+with open("C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\puzzle_4\\input.txt", "r") as f:
+    data_raw = f.read().strip().split("\n")
 
-reports = [list(map(int,line.split())) for line in data_raw.strip().split("\n")]
+reports = [list(map(int, line.split())) for line in data_raw]
 
-def is_safe(report): 
+def is_safe(report):
     differences = [report[i+1] - report[i] for i in range(len(report)-1)]
-    increasing = all(0<diff<=3 for diff in differences)
-    decreasing = all(-3<=diff<0 for diff in differences)
-    return increasing or decreasing 
+    return all(0 < diff <= 3 for diff in differences) or all(-3 <= diff < 0 for diff in differences)
 
 def damper(report):
-    if is_safe(report):
+    n = len(report)
+    differences = [report[i+1] - report[i] for i in range(n-1)]
+
+    if all(0 < diff <= 3 for diff in differences) or all(-3 <= diff < 0 for diff in differences):
         return True
-    
-    for i in range(len(report)):
-        new_report = report[:i] + report[i+1:]
-        if is_safe(new_report):
+
+    for i in range(n):
+        if i == 0:
+            new_diff = differences[1:]
+        elif i == n-1:
+            new_diff = differences[:-1]
+        else:
+            new_diff = differences[:i-1] + [report[i+1] - report[i-1]] + differences[i+1:]
+        
+        if all(0 < diff <= 3 for diff in new_diff) or all(-3 <= diff < 0 for diff in new_diff):
             return True
     
     return False
