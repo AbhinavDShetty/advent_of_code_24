cbf840a7e01678ea7f6e8797b2041a4361fc5767
AbhinavDShetty <abhinavdshetty@gmail.com>
2024-12-05 15:07:56 +0530

    Day5 Solutions

---
+++ Solution_Python/day_5/part_2/sol_2.py
@@ -0,0 +1,33 @@
+with open('C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\Solution_Python\\day_5\\input.txt', 'r') as f:
+    data = f.read()
+
+
+def input(data):
+    rules_raw, updates_raw = data.strip().split('\n\n')
+    rules = []
+    for rule in rules_raw.splitlines():
+        x, y = map(int, rule.split('|'))
+        rules.append((x, y))
+
+    updates = [list(map(int, update.split(','))) for update in updates_raw.splitlines()]
+    return rules, updates
+
+
+def correct_updates(rules, update):
+    dic = {page: i for i, page in enumerate(update)}
+    for x, y in rules:
+        if x in dic and y in dic:
+            if dic[y] < dic[x]:
+                return False
+    return True
+
+
+
+
+rules, updates = input(data)
+mid_sum = 0
+for update in updates:
+    if correct_updates(rules, update):
+        mid_sum +=  update[len(update) // 2]
+
+print(mid_sum)
