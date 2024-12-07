43c94b1c70c85507c385be2cea94f46d151d0beb
AbhinavDShetty <abhinavdshetty@gmail.com>
2024-12-07 14:00:21 +0530

    Day7 Solutions

---
+++ Solution_Python/day_7/part_1/sol_1.py
@@ -0,0 +1,33 @@
+import itertools
+
+with open('C:\\Users\\deepa\\OneDrive\\Documents\\GitHub\\advent_of_code_24\\Solution_Python\\day_7\\input.txt', 'r') as f:
+    data = f.readlines()
+
+
+equations = []
+for line in data:
+    target, numbers = line.split(":")
+    target = int(target.strip())
+    numbers = list(map(int, numbers.strip().split()))
+    equations.append((target, numbers))
+
+def calculate_possibility(numbers, target):
+    
+    possible = False
+    operation = ['+','*']
+    
+    for operators in itertools.product(operation, repeat = len(numbers) - 1):
+        result = numbers[0]
+        for i, operator in enumerate(operators):
+            if operator == '+':
+                result += numbers[i+1]
+            elif operator == '*':
+                result *= numbers[i+1]
+        if result == target:
+            possible = True
+            break
+    
+    return possible
+
+total_sum = sum(target for target, numbers in equations if calculate_possibility(numbers, target))
+print(total_sum)
