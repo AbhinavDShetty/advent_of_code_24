514b3951b4105ceab9cd96beddf35564b23d4b90
AbhinavDShetty <abhinavdshetty@gmail.com>
2025-01-30 09:47:50 +0530

    Day 16 Part1 Solution

--- day_16/part1.py
+++ day_16/part1.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from collections import deque
 
-file_path = Path(__file__).parent / "input1.txt"
+file_path = Path(__file__).parent / "input2.txt"
 
 def read_file():
     with open(file_path, 'r') as f:
@@ -10,10 +10,12 @@ def read_file():
 
 
 def print_maze(maze):
+    # for row in maze:
+    #     for column in row:
+    #         print(column, end='')
+    #     print()
     for row in maze:
-        for column in row:
-            print(column, end='')
-        print()
+        print(row)
 
 
 directions = [(1, 0), (0, -1), (-1, 0), (0, 1)] # Down Left Up Right
@@ -25,11 +27,40 @@ def turn_right(current_direction):
 
 def turn_left(current_direction):
     return directions[(directions.index(current_direction) - 1) % 4]
-maze = read_file()
-print(maze[len(maze) - 2][1])
+
+
 def bfs(maze):
     queue = deque()
     start = (len(maze) - 2, 1, (0, 1), 0)
+    maze[len(maze)-2][1] = 0
     queue.append(start)
     
     while queue:
+        current = queue.popleft()
+        cur_x, cur_y, cur_dir, cur_score = current[0], current[1], current[2], current[3]
+        
+        allowed_directions_score = [(cur_dir, cur_score + 1), (turn_left(cur_dir), cur_score + 1001), (turn_right(cur_dir), cur_score + 1001)]
+        
+        for new_dir, new_score in allowed_directions_score:
+            new_x, new_y = cur_x + new_dir[0], cur_y + new_dir[1]
+            
+            if maze[new_x][new_y] == '#':
+                continue
+            
+            if maze[new_x][new_y] in ['.','E'] or (isinstance(maze[new_x][new_y], int) and maze[new_x][new_y]> new_score):
+                maze[new_x][new_y] = new_score
+                queue.append((new_x, new_y, new_dir, new_score))
+            # print_maze(maze)
+        # print_maze(maze)
+        
+    return maze[1][len(maze[1])-2]
+
+
+def resolve():
+    maze = read_file()
+    result = bfs(maze)
+    print("The Final Maze is:\n")
+    print_maze(maze)
+    return result
+
+print(resolve())
